64fc77860dc46b9332bb8b3ce1482a0db65b2c9b
YuuyuMP <yuyumpoptec@gmail.com>
2022-09-26 10:53:23 -0500

    feat: uso de script externo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Saoko papi saoko");
-      function colorTitulo(){
-        console.log("Pulse botón");
-        document.getElementById("generation").style.color = "plum";
-      }
+
+function colorTitulo(){
+    console.log("Pulse botón");
+    document.getElementById("generation").style.color = "plum";
+}
